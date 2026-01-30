4af0f6750edd77018aa062e2076261090746bbe3
elecbug <deveb1479@gmail.com>
2026-01-30 17:26:47 +0900

    feat: add header

--- init.js
+++ init.js
@@ -1,7 +1,22 @@
+header = `
+<h1>
+    <a href="index.html" style="text-decoration: none; color: inherit;">
+        <img src="figure/icon.png" alt="Article SAVE Logo" style="height: 50px; vertical-align: middle; margin-right: 10px;">
+        Article SAVE
+    </a>
+</h1>
+`;
+
 footer = `
 <p>
     © 2026 <a href="https://github.com/elecbug">elecbug</a> in <a href="https://github.com/kmu-save">KMU SAVE</a>. All rights reserved.
 </p>
 `;
 
+nav = `
+
+`;
+
+document.getElementById('header').innerHTML = header;
 document.getElementById('footer').innerHTML = footer;
+document.getElementById('nav').innerHTML = nav;
